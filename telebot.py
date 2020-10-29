7e20a5693584ea55a7ee02473986248ff4cad493
John Trần <31305653+bittervirgin@users.noreply.github.com>
2020-10-29 22:36:01 +0700

    Add DAU

--- telebot.py
+++ telebot.py
@@ -5,19 +5,19 @@ import time
 import json
 import os.path
 merchant_list = [660774, 373138]
-group_id = "-473360139"
+group_id = ["-473360139", "-334458180"]
 personal_id = "1199485011"
-def api_call(text):
+def api_call(text, chat_id):
     url = "https://api.telegram.org/bot"
     token = "1253557076:AAGiiX_5xSfDz6PqPit8MuzuTU0A8MhczKk"
-    chat_id = group_id #chat_id of person or group you want to send messsages
+    chat_id = personal_id #chat_id of person or group you want to send messsages
 
     endpoint = url + token + "/sendMessage?chat_id=" + chat_id + "&text=" + text
     response = requests.get(endpoint)
 
     return response
 
-def generate_report(merchant_id):
+def generate_report():
     print("report")
     #get yesterday date
     yesterday = test.process_date()
@@ -35,46 +35,38 @@ def generate_report(merchant_id):
 
     #check if json file is available
     start_time = time.time()
-    if (os.path.isfile('orders_{0}_{1}.json'.format(yesterday,merchant_id)) == False):
-        test.save_json("orders", merchant_id)
-        orders = json.loads(test.open_file("orders",merchant_id))
-    else: orders = json.loads(test.open_file("orders",merchant_id))
-    end_time = time.time()
-    print("Time checking json file: ", end_time-start_time)
+    if (os.path.isfile('orders_{0}.json'.format(str(test.process_date()))) == False):
+        test.save_json("orders")
+        orders = json.loads(test.open_file("orders"))
+    else: orders = json.loads(test.open_file("orders"))
     #orders = json.loads(test.open_file("orders"))
-
-
     #Processing report
-    start_time = time.time()
     data = orders['data']
     count = data['count']
+    count_test = 0
     jobs = data['all_jobs']
     #total = test.get_quantity(merchant_id)
-    total = 0
     revenue = 0
+    '''
     if merchant_id == 660774:
         merchant_name = "Phú Mỹ Hưng"
     else: merchant_name = "Era Town"
-    quantity = 0
-    for i in range(len(jobs)):
-        #print(i)
-        order = jobs[i]
-        dtails = order['product_details']
-        #print(dtails)
-        for j in range(len(dtails)):
-            product = str(dtails[j])
-            quantity  = product.split('$#@')
-           # print(quantity)
-            total += float(quantity[1])
-    
+    '''
+    total_quantity = test.get_quantity()
+    count = count - count_test
     order_amount = sum([order.get('order_amount', 0) for order in jobs])
     revenue = "{:,.0f}".format(order_amount)
-    basket_size = total/count
+    basket_size = total_quantity/count
     ticket_size = int(order_amount)/count
     ticket_size = "{:,.2f}".format(ticket_size)
+    #Customer development
+    new_user, sign_up = test.first_order_counting()
+    print(sign_up)
+    DAU = test.daily_active_user()
     #create message content
-    text =  "\t DAILY UPDATE " + str(yesterday) + "\n" + "SALES METRICS (" + merchant_name + ")  \n"   + "Revenue: " + str(revenue) + "\nOrders: " + str(count) + "\nTicket size: " + str(ticket_size) + "\nBasket size: " + str(round(basket_size, 2))
+    text =  "\t DAILY UPDATE " + str(yesterday) + "\n" + "SALES METRICS\n" + "Revenue: " + str(revenue) + "\nOrders: " + str(count) + "\nTicket size: " + str(ticket_size) + "\nBasket size: " + str(round(basket_size, 2)) + "\nCUSTOMER DEVELOPMENT" + "\nDaily Active User: "+ str(DAU)  +"\nNew user: " + str(new_user) +"\nConversion rate: "+ str(round(new_user/sign_up, 2))
     end_time = time.time()
+
     print("Generate report time:", end_time-start_time)
     return text
 
@@ -89,9 +81,8 @@ if  __name__ == "__main__":
     print(yesterday)
     text = "#DAILY UPDATE " + str(yesterday) + "\n"
     #api_call(text)
-    for i in range(len(merchant_list)):
-        user_id = merchant_list[i]
-        api_call(generate_report(user_id))
+    for id in group_id:   
+        api_call(generate_report(), id)
     
 
     
--- test.py
+++ test.py
@@ -21,11 +21,11 @@ def process_date():
     
     return yesterday
 
-def api_call(user_id, start_date, end_date):
+def api_call(start_date, end_date):
     url = "https://api.yelo.red/open/orders/getAll"
     parameter = {
         "api_key": api_key,
-        "user_id": user_id,
+        "user_id": 660774,
         "marketplace_user_id": 373138,
         "order_status": 13,
         "start": 0,
@@ -42,7 +42,7 @@ def user_details():
     parameter = {
         "api_key": "aad87082b5e5cde09257c29948bb37b0",
         "marketplace_user_id": 373138,
-        "length": 1500
+        "length": 2000
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url, params = parameter, headers=headers)
@@ -82,12 +82,12 @@ def jsave(obj):
     #with open('data20201009.txt', 'w') as outfile:
         #json.dump(text, outfile)
     
-def save_json(obj, user_id):
+def save_json(obj):
     yesterday = str(process_date())
     if obj == "orders":
         
-        with open('orders_{0}_{1}.json'.format(str(yesterday),str(user_id)), 'w')  as outfile:
-            json.dump(api_call(user_id, yesterday, yesterday).json(), outfile)
+        with open('orders_{0}.json'.format(str(yesterday)), 'w')  as outfile:
+            json.dump(api_call(yesterday, yesterday).json(), outfile)
         print("Success")
     if obj == "customer":
         with open('users_%s.json' %yesterday, 'w') as outfile:
@@ -100,10 +100,10 @@ def save_json(obj, user_id):
         #print("1")
     '''
 
-def open_file(ftype, user_id):
+def open_file(ftype):
     if ftype == "orders":
         yesterday = process_date()
-        with open('orders_{0}_{1}.json'.format(str(yesterday),str(user_id)), 'r') as f:
+        with open('orders_{0}.json'.format(str(yesterday)), 'r') as f:
             orders = f.read()
 
     return orders
@@ -113,7 +113,7 @@ def get_list_order(user_id):
     yesterday = str(process_date())
     list_orders = []
     #save_json("orders", user_id)
-    all_orders = api_call(user_id, yesterday, yesterday).json()
+    all_orders = api_call(yesterday, yesterday).json()
     data = all_orders['data']
     jobs = data['all_jobs']
     for i in range(len(jobs)):
@@ -122,25 +122,33 @@ def get_list_order(user_id):
         list_orders.append(order['job_id'])
     return list_orders
 
-
 #get total quantity 
-def get_quantity(user_id):
-    print("Quantity")
+def get_quantity():
+    total = 0
+    yesterday = process_date()
+    with open('orders_{0}.json'.format(str(yesterday)), 'r') as f:
+        all_orders = json.loads(f.read())
+    print(type(all_orders))
+    data = all_orders['data']
+    count = data['count']
+    count_test = 0
+    jobs = data['all_jobs']
     quantity = 0
-    list_orders = get_list_order(user_id)
-    for i in range(len(list_orders)):
-        order = order_details(list_orders[i]).json()
-        data = order['data']
-        #print(type(data))
-        #jprint(data)
-        data1 = data[0]
-        data = data1['orderDetails']
-        for i in range(len(data)):
-            #print(i)
-            order_detail = data[i]
-            product = order_detail['product']
-            quantity += product['quantity']
-    return quantity
+    for i in range(len(jobs)):
+        #print(i)
+        order = jobs[i]
+        if ("Test" in order['customer_username']):
+            count_test += 1
+        else:
+            dtails = order['product_details']
+        #print(dtails)
+            for j in range(len(dtails)):
+                product = str(dtails[j])
+                quantity  = product.split('$#@')
+                print(quantity)
+                
+                total += float(quantity[1])
+    return total
 
 def get_promo_list():
     url = "https://api.yelo.red/open/promo/get"
@@ -154,7 +162,6 @@ def get_promo_list():
     response = requests.post(url, data=json.dumps(params), headers=header)
     return response
 
-#jprint(user_details().json())
 def order_call():
     url = "https://api.yelo.red/open/orders/getAll"
     parameter = {
@@ -171,38 +178,30 @@ def order_call():
     response = requests.post(url, data=json.dumps(parameter), headers=headers)
     return response
 
-#jprint(user_details().json())
+#GET HOW MANY CUSTOMER HAVE FIRST ORDER
 def first_order_counting():
-
-    users = user_details().json()
+    year, month,day = get_d_m_y()
+    users = process_user_data()
     data = users['data']
-    #jprint(data)\
     j = 0
-    #wb = Workbook()
-    #sheet1 = wb.add_sheet('Sheet 1')
     user_list = []
     print(type(user_list))
     for i in range(len(data)):
         details = data[i]
         datetime = details['creation_datetime']
-        #sheet1.write(i, 2, details['creation_datetime'])
         dt = str(datetime).split('T')
         ymd = dt[0].split('-')
         temp = {}
-        if (int(ymd[1]) >= 10):
-            if ( int(ymd[2]) >= 10 ):
+        if (int(ymd[1]) >= int(month)):
+            if ( int(ymd[2]) >= int(day) ):
                 j += 1
                 temp['name'] = details['name']
                 temp['id'] = details['vendor_id']
                 user_list.append(temp)
-            #sheet1.write(i, 0, details['name'])
-            #sheet1.write(i, 1, details['phone_no'])
-    #print(user_list)
-    start_date = "20201010"
+    start_date = str(process_date())
     end_date = str(process_date())
-    orders_list = api_call(660774, start_date, end_date).json()
+    orders_list = api_call(start_date, end_date).json()
     data = orders_list['data']
-    #count = data['count']
     jobs = data['all_jobs']
     amount = 0
     user_id_list = list(set([user['id'] for user in user_list]))
@@ -212,7 +211,71 @@ def first_order_counting():
     a = {k: v for k, v in sorted(a.items(), key=lambda item: item[1], reverse=True)}
     print(a)
     amount = len([x for x in a if a[x] == 2])
-    print(amount)
-    #wb.save('regis user.xls')
-first_order_counting()
+    #print([x for x in a if a[x] == 2])
+    #first_user = list(set())
 
+    #print(amount)
+    #return number of first order and total signed up users
+    return amount, j
+
+#GET DISTINCT DAY MONTH YEAR OF DATE
+def get_d_m_y():
+    yesterday = process_date()
+    year = yesterday[0:4]
+    month = yesterday[4:6]
+    day = yesterday[6:8]
+    return year,month,day
+
+#GET ALL ORDERS
+def api_call1():
+    url = "https://api.yelo.red/open/orders/getAll"
+    parameter = {
+        "api_key": api_key,
+        "marketplace_user_id": 373138,
+        "order_status": 13,
+        "start": 0,
+        "length": 6000
+    } 
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, data=json.dumps(parameter), headers=headers)
+    return response
+
+def Tran_requirement():
+    all_orders = api_call1().json()
+    data = all_orders['data']
+    count = data['count'] #total number of orders
+    jobs = data['all_jobs']
+    #get list unique customer list from the begining till now
+    customer_id = list(set([job['customer_id'] for job in jobs]))
+    total = 0 
+    [total :=  total + job['order_amount'] for job in jobs] #total amount of every order since the begining
+    print("A = ", len(customer_id))
+    print("B = ", count)
+    print("D = ", total)
+    print("C = B / A = ", count / len(customer_id))
+    print("E = D / A = ", total / len(customer_id))
+
+def process_user_data():
+    yesterday = process_date()
+    with open('users_{0}.json'.format(str(yesterday)), 'w') as outfile:
+        json.dump(user_details().json(), outfile)
+
+    with open('users_{0}.json'.format(str(yesterday)), 'r') as f:
+        all_users = json.loads(f.read())
+    return all_users
+
+def recurring_user():
+    pass
+
+first_order_counting()
+def daily_active_user():
+    yesterday = process_date()
+    with open('orders_{0}.json'.format(str(yesterday)), 'r')  as outfile:
+        all_orders = json.loads(outfile.read())
+
+    data = all_orders['data']
+    jobs = data['all_jobs']
+    user_id_list = list(set([job['customer_id'] for job in jobs]))
+    return len(user_id_list)
+
+    
